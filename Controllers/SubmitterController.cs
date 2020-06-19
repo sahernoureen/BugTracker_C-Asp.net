4507805af0de4acd07b8dc47221dfa1a39e80be6
Christian <chris113377@gmail.com>
2020-06-19 09:33:42 -0500

    Added controller for users

---
+++ Controllers/SubmitterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTracker.Controllers
+{
+    public class SubmitterController : Controller
+    {
+        // GET: Submitter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
